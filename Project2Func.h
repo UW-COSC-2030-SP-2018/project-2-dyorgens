fcd8e3b5317344b47eb2e3c66f96f9128b548325
dyorgens <35741856+dyorgens@users.noreply.github.com>
2018-05-06 15:54:47 -0600

    Binary Search and Merge Sort Added

--- Project2Func.h
+++ Project2Func.h
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -25,6 +26,7 @@ void quickSort(vector<int>& A, int p, int q)
 		quickSort(A, r + 1, q);
 	}
 }
+
 int partition(vector<int>& A, int p, int q)
 {
 	int x = A[p];
@@ -41,3 +43,34 @@ int partition(vector<int>& A, int p, int q)
 	swap(A[i], A[p]);
 	return i;
 }
+//End of QuickSort
+
+//Binary search function 
+int binary_search(vector<int> v, int data) {
+	auto it = lower_bound(v.begin(), v.end(), data);
+	if (it == v.end() || *it != data) {
+		return -1;
+	}
+	else {
+		std::size_t index = distance(v.begin(), it);
+		return index;
+	}
+}
+//Beginning of MergeSort
+vector<int> merge_sort(const vector<int>& input)
+{
+	if (input.size() <= 1) return input;
+	vector<int> output(input.size());
+
+	//Split Vector//
+	int midpoint = 0.5*input.size();
+	vector<int> input_left(input.begin(), input.begin() + midpoint);
+	vector<int> input_right(input.begin() + midpoint, input.end());
+
+	input_left = merge_sort(input_left);
+	input_right = merge_sort(input_right);
+	merge(input_left.begin(), input_left.end(), input_right.begin(), input_right.end(), output.begin());
+
+	return output;
+}
+//Beginning of Integer Hash Fucntion
